693e1e7ed3bb37aa2ca79946b96e249605cc7c55
hpintro <hpintro@gmail.com>
2014-01-06 17:41:01 +0700

    Demo getCustomer in excel file

---
+++ Server/DMSProject/src/java/com/hp/excelhandle/Customer.java
@@ -0,0 +1,253 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hp.excelhandle;
+
+/**
+ *
+ * @author HP
+ */
+public class Customer extends Object{
+    private int mStt;
+    private String mTinhThanh;
+    private String mTuyenBanHangThu;
+    private String mMaNhanVien;
+    private String mX;
+    private String mMaDoiTuong;
+    private String mDoiTuong;
+    private double mNoDKy;
+    private double mCoDKy;
+    private double mNoTKy;
+    private double mTienBan;
+    private double mCoTKy;
+    private double mCKGG;
+    private double mNhapLai;
+    private double mNoCKy;
+    private double mCoCKy;
+    private double mDoanhThu;
+    private int mPhanTramNoChiaThu;
+    private double mNoToiDa;
+    private String mDaiDien;
+    private String mDiaChi;
+    private String mDienThoai;
+    private String mFax;
+    private String mGhiChu;
+           
+    private double mXCoordinates;
+    private double mYCoordinates;
+
+    public int getmStt() {
+        return mStt;
+    }
+
+    public void setmStt(int mStt) {
+        this.mStt = mStt;
+    }
+
+    public String getmTinhThanh() {
+        return mTinhThanh;
+    }
+
+    public void setmTinhThanh(String mTinhThanh) {
+        this.mTinhThanh = mTinhThanh;
+    }
+
+    public String getmTuyenBanHangThu() {
+        return mTuyenBanHangThu;
+    }
+
+    public void setmTuyenBanHangThu(String mTuyenBanHangThu) {
+        this.mTuyenBanHangThu = mTuyenBanHangThu;
+    }
+
+    public String getmMaNhanVien() {
+        return mMaNhanVien;
+    }
+
+    public void setmMaNhanVien(String mMaNhanVien) {
+        this.mMaNhanVien = mMaNhanVien;
+    }
+
+    public String getmX() {
+        return mX;
+    }
+
+    public void setmX(String mX) {
+        this.mX = mX;
+    }
+
+    public String getmMaDoiTuong() {
+        return mMaDoiTuong;
+    }
+
+    public void setmMaDoiTuong(String mMaDoiTuong) {
+        this.mMaDoiTuong = mMaDoiTuong;
+    }
+
+    public String getmDoiTuong() {
+        return mDoiTuong;
+    }
+
+    public void setmDoiTuong(String mDoiTuong) {
+        this.mDoiTuong = mDoiTuong;
+    }
+
+    public double getmNoDKy() {
+        return mNoDKy;
+    }
+
+    public void setmNoDKy(double mNoDKy) {
+        this.mNoDKy = mNoDKy;
+    }
+
+    public double getmCoDKy() {
+        return mCoDKy;
+    }
+
+    public void setmCoDKy(double mCoDKy) {
+        this.mCoDKy = mCoDKy;
+    }
+
+    public double getmNoTKy() {
+        return mNoTKy;
+    }
+
+    public void setmNoTKy(double mNoTKy) {
+        this.mNoTKy = mNoTKy;
+    }
+
+    public double getmTienBan() {
+        return mTienBan;
+    }
+
+    public void setmTienBan(double mTienBan) {
+        this.mTienBan = mTienBan;
+    }
+
+    public double getmCoTKy() {
+        return mCoTKy;
+    }
+
+    public void setmCoTKy(double mCoTKy) {
+        this.mCoTKy = mCoTKy;
+    }
+
+    public double getmCKGG() {
+        return mCKGG;
+    }
+
+    public void setmCKGG(double mCKGG) {
+        this.mCKGG = mCKGG;
+    }
+
+    public double getmNhapLai() {
+        return mNhapLai;
+    }
+
+    public void setmNhapLai(double mNhapLai) {
+        this.mNhapLai = mNhapLai;
+    }
+
+    public double getmNoCKy() {
+        return mNoCKy;
+    }
+
+    public void setmNoCKy(double mNoCKy) {
+        this.mNoCKy = mNoCKy;
+    }
+
+    public double getmCoCKy() {
+        return mCoCKy;
+    }
+
+    public void setmCoCKy(double mCoCKy) {
+        this.mCoCKy = mCoCKy;
+    }
+
+    public double getmDoanhThu() {
+        return mDoanhThu;
+    }
+
+    public void setmDoanhThu(double mDoanhThu) {
+        this.mDoanhThu = mDoanhThu;
+    }
+
+    public int getmPhanTramNoChiaThu() {
+        return mPhanTramNoChiaThu;
+    }
+
+    public void setmPhanTramNoChiaThu(int mPhanTramNoChiaThu) {
+        this.mPhanTramNoChiaThu = mPhanTramNoChiaThu;
+    }
+
+    public double getmNoToiDa() {
+        return mNoToiDa;
+    }
+
+    public void setmNoToiDa(double mNoToiDa) {
+        this.mNoToiDa = mNoToiDa;
+    }
+
+    public String getmDaiDien() {
+        return mDaiDien;
+    }
+
+    public void setmDaiDien(String mDaiDien) {
+        this.mDaiDien = mDaiDien;
+    }
+
+    public String getmDiaChi() {
+        return mDiaChi;
+    }
+
+    public void setmDiaChi(String mDiaChi) {
+        this.mDiaChi = mDiaChi;
+    }
+
+    public String getmDienThoai() {
+        return mDienThoai;
+    }
+
+    public void setmDienThoai(String mDienThoai) {
+        this.mDienThoai = mDienThoai;
+    }
+
+    public String getmFax() {
+        return mFax;
+    }
+
+    public void setmFax(String mFax) {
+        this.mFax = mFax;
+    }
+
+    public String getmGhiChu() {
+        return mGhiChu;
+    }
+
+    public void setmGhiChu(String mGhiChu) {
+        this.mGhiChu = mGhiChu;
+    }
+
+    public double getmXCoordinates() {
+        return mXCoordinates;
+    }
+
+    public void setmXCoordinates(double mXCoordinates) {
+        this.mXCoordinates = mXCoordinates;
+    }
+
+    public double getmYCoordinates() {
+        return mYCoordinates;
+    }
+
+    public void setmYCoordinates(double mYCoordinates) {
+        this.mYCoordinates = mYCoordinates;
+    }
+    
+    public Customer(){
+        
+    }
+}
--- Server/DMSProject/src/java/com/hp/excelhandle/GetData.java
+++ Server/DMSProject/src/java/com/hp/excelhandle/GetData.java
@@ -7,22 +7,31 @@
 package com.hp.excelhandle;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 
+import com.hp.common.ConfigFile;
 
 /**
  *
  * @author HP
  */
 public class GetData {
+        
+    private String mFileInput;
     
     public GetData(){
         
     }
+    
+    public GetData(String pFileInput){
+        this.mFileInput = pFileInput;
+    }
+    
     public void readExcel(){
         try {
             POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream("database/customer.xls"));
@@ -58,14 +67,9 @@ public class GetData {
 //                    }
 //                }
 //            }
+            
             row = sheet.getRow(8);
-            if(row != null){
-//                for(int c = 0; c < cols; c++){
-//                    cell = row.getCell((short)c);
-//                    if(cell != null)
-//                        System.out.println("Row: " + c + ", Data: " + cell.getStringCellValue());
-//                }
-                
+            if(row != null){               
                 cell = row.getCell(1);
                     if(cell != null)
                         System.out.println("Row: " + 9 + ", Data: " + cell.getStringCellValue());
@@ -77,7 +81,67 @@ public class GetData {
         }
     }
     
+    public ArrayList<Customer> loadCustomer(){
+        ArrayList<Customer> listCustomer = new ArrayList();
+        
+        try {
+            POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(mFileInput));
+            HSSFWorkbook wb = new HSSFWorkbook(fs);
+            HSSFSheet sheet = wb.getSheetAt(0);
+            HSSFRow row;
+            HSSFCell cell;
+
+            int rows; // No of rows
+            rows = sheet.getPhysicalNumberOfRows();
+
+            int cols = 0; // No of columns (max)
+            int tmp = 0;
+
+            // This trick ensures that we get the data properly even if it doesn't start from first few rows
+            for(int i = 0; i < 10 || i < rows; i++) {
+                row = sheet.getRow(i);
+                if(row != null) {
+                    tmp = sheet.getRow(i).getPhysicalNumberOfCells();
+                    if(tmp > cols) 
+                        cols = tmp;
+                }
+            }
+
+            for(int i = 8; i < rows; i++){
+                
+                row = sheet.getRow(i);
+                if(row != null){
+                    
+                    //If the customer id null
+                    if(row.getCell(ConfigFile.MA_DOI_TUONG_COL) == null ||
+                            row.getCell(ConfigFile.X_COORDINATES_COL) == null ||
+                            row.getCell(ConfigFile.Y_COORDINATES_COL) == null
+                            ){
+                        continue;   
+                    }
+                    
+                    //Init Customer Object
+                    Customer custumer = new Customer();
+                    custumer.setmXCoordinates(row.getCell(ConfigFile.X_COORDINATES_COL).getNumericCellValue());
+                    custumer.setmXCoordinates(row.getCell(ConfigFile.Y_COORDINATES_COL).getNumericCellValue());
+                    
+                    listCustomer.add(custumer);
+                    System.out.println("Add Object " + i);
+                }
+            }
+            
+        } catch(Exception ioe) {
+            ioe.printStackTrace();
+            return null;
+        }
+        
+        return listCustomer;
+    }
+    
     public static void main(String [] arg){
-        new GetData().readExcel();
+        GetData data= new GetData("database/customer.xls");
+        Customer cus = data.loadCustomer().get(0);
+        
+        System.out.println("Customer 1: X:" + cus.getmXCoordinates() +", Y: "+ cus.getmYCoordinates());
     }
 }

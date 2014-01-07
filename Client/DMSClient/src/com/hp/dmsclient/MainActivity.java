65995b62321185fa440c7b111b32e3ab02fe830e
hpintro <hpintro@gmail.com>
2014-01-08 03:30:53 +0700

    Get location in client ( a phone)

--- Client/DMSClient/src/com/hp/dmsclient/MainActivity.java
+++ Client/DMSClient/src/com/hp/dmsclient/MainActivity.java
@@ -1,23 +1,87 @@
 package com.hp.dmsclient;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener{
+	
+	LocationManager locationManager ;
+	String provider;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Getting LocationManager object
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);        
+        
+        // Creating an empty criteria object
+        Criteria criteria = new Criteria();
+        
+        // Getting the name of the provider that meets the criteria
+        provider = locationManager.getBestProvider(criteria, false);
+        
+                
+        if(provider!=null && !provider.equals("")){
+        	
+        	// Get the location from the given provider 
+            Location location = locationManager.getLastKnownLocation(provider);
+                        
+            locationManager.requestLocationUpdates(provider, 20000, 1, this);
+            
+            
+            if(location!=null)
+            	onLocationChanged(location);
+            else
+            	Toast.makeText(getBaseContext(), "Location can't be retrieved", Toast.LENGTH_SHORT).show();
+            
+        }else{
+        	Toast.makeText(getBaseContext(), "No Provider Found", Toast.LENGTH_SHORT).show();
+        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
     
+	@Override
+	public void onLocationChanged(Location location) {
+		// Getting reference to TextView tv_longitude
+		TextView tvLongitude = (TextView)findViewById(R.id.tv_longitude);
+		
+		// Getting reference to TextView tv_latitude
+		TextView tvLatitude = (TextView)findViewById(R.id.tv_latitude);
+		
+		// Setting Current Longitude
+		tvLongitude.setText("Longitude:" + location.getLongitude());
+		
+		// Setting Current Latitude
+		tvLatitude.setText("Latitude:" + location.getLatitude() );
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub		
+	}
 }

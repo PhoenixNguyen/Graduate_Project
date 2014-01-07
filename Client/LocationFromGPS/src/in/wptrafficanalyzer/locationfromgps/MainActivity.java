e7d54101d619396fd1547122593c583c4053c4be
a1hung1 <tranmanhhungcntt2@gmail.com>
2014-01-07 12:52:10 -0800

    update location

--- Client/LocationFromGPS/src/in/wptrafficanalyzer/locationfromgps/MainActivity.java
+++ Client/LocationFromGPS/src/in/wptrafficanalyzer/locationfromgps/MainActivity.java
@@ -13,88 +13,88 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements LocationListener {
-
-	LocationManager locationManager;
+public class MainActivity extends Activity implements LocationListener{
+	
+	LocationManager locationManager ;
 	String provider;
-	Button bt;
 	TextView tvLongitude;
 	TextView tvLatitude;
+	Button bt;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		bt = (Button) findViewById(R.id.button1);
-		bt.setOnClickListener(new View.OnClickListener() {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bt.setOnClickListener(new View.OnClickListener() {
+			
 			@Override
 			public void onClick(View v) {
-				tvLongitude.setText(text)
+				// TODO Auto-generated method stub
+				
 			}
 		});
-		setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        
+        // Getting LocationManager object
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);        
+        
+        // Creating an empty criteria object
+        Criteria criteria = new Criteria();
+        
+        // Getting the name of the provider that meets the criteria
+        provider = locationManager.getBestProvider(criteria, false);
+        
+                
+        if(provider!=null && !provider.equals("")){
+        	
+        	// Get the location from the given provider 
+            Location location = locationManager.getLastKnownLocation(provider);
+                        
+            locationManager.requestLocationUpdates(provider, 20000, 1, this);
+            
+            
+            if(location!=null)
+            	onLocationChanged(location);
+            else
+            	Toast.makeText(getBaseContext(), "Location can't be retrieved", Toast.LENGTH_SHORT).show();
+            
+        }else{
+        	Toast.makeText(getBaseContext(), "No Provider Found", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-		// Getting LocationManager object
-		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-		// Creating an empty criteria object
-		Criteria criteria = new Criteria();
-
-		// Getting the name of the provider that meets the criteria
-		provider = locationManager.getBestProvider(criteria, false);
-
-		if (provider != null && !provider.equals("")) {
-
-			// Get the location from the given provider
-			Location location = locationManager.getLastKnownLocation(provider);
-
-			locationManager.requestLocationUpdates(provider, 20000, 1, this);
-
-			if (location != null)
-				onLocationChanged(location);
-			else
-				Toast.makeText(getBaseContext(), "Location can't be retrieved",
-						Toast.LENGTH_SHORT).show();
-
-		} else {
-			Toast.makeText(getBaseContext(), "No Provider Found",
-					Toast.LENGTH_SHORT).show();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
 	@Override
 	public void onLocationChanged(Location location) {
 		// Getting reference to TextView tv_longitude
-		tvLongitude = (TextView) findViewById(R.id.tv_longitude);
-
+		tvLongitude = (TextView)findViewById(R.id.tv_longitude);
+		
 		// Getting reference to TextView tv_latitude
-		tvLatitude = (TextView) findViewById(R.id.tv_latitude);
-
+		tvLatitude = (TextView)findViewById(R.id.tv_latitude);
+		
 		// Setting Current Longitude
 		tvLongitude.setText("Longitude:" + location.getLongitude());
-
+		
 		// Setting Current Latitude
-		tvLatitude.setText("Latitude:" + location.getLatitude());
+		tvLatitude.setText("Latitude:" + location.getLatitude() );
 	}
 
 	@Override
 	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
 	}
 
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
 	}
 }

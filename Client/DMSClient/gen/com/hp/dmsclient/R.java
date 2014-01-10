0ea0023048299756397576b85bf6bd26e1b7eeb5
a1hung1 <tranmanhhungcntt2@gmail.com>
2014-01-10 07:01:23 -0800

    add take_photo

--- Client/DMSClient/gen/com/hp/dmsclient/R.java
+++ Client/DMSClient/gen/com/hp/dmsclient/R.java
@@ -15,10 +15,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070003;
+        public static final int btnIntend=0x7f070003;
+        public static final int btnIntendS=0x7f070004;
+        public static final int btnIntendV=0x7f070005;
+        public static final int imageView1=0x7f070006;
+        public static final int menu_settings=0x7f070008;
         public static final int tv_latitude=0x7f070002;
         public static final int tv_location=0x7f070000;
         public static final int tv_longitude=0x7f070001;
+        public static final int videoView1=0x7f070007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -27,7 +32,12 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int album_name=0x7f040009;
         public static final int app_name=0x7f040000;
+        public static final int btnIntend=0x7f040005;
+        public static final int btnIntendS=0x7f040006;
+        public static final int btnIntendV=0x7f040007;
+        public static final int cannot=0x7f040008;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int str_tv_location=0x7f040004;

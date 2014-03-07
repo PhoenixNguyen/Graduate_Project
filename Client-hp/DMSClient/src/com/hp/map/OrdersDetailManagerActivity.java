a28f7920c55f681e1dc308275a137a189e726ecc
hpintro <hpintro@gmail.com>
2014-03-07 19:58:08 +0700

    inteface for detail orders

--- Client-hp/DMSClient/src/com/hp/map/OrdersDetailManagerActivity.java
+++ Client-hp/DMSClient/src/com/hp/map/OrdersDetailManagerActivity.java
@@ -3,6 +3,7 @@ package com.hp.map;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable.Orientation;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.widget.ImageButton;
@@ -24,23 +25,78 @@ public class OrdersDetailManagerActivity extends Activity{
 	}
 	
 	public void addTable(){
-		layout = (TableLayout)findViewById(R.id.detail_list);
+		layout = (LinearLayout)findViewById(R.id.detail_list);
 		
 		for(int i = 0; i < 10; i++){
+			
+			int bg= 0;
+			if(i%2 == 0){
+				bg = R.drawable.table_border;
+			}
+			else
+				bg = R.drawable.table_border2;
+			
 			TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
 			
 			TableLayout tb = new TableLayout(this);
 			tb.setLayoutParams(lp);
+			tb.setOrientation(TableLayout.VERTICAL);
+			tb.setBackgroundResource(bg);
 			
+			//NEW ROW
 			TableRow tbRow1 = new TableRow(this);
 			tbRow1.setLayoutParams(lp);
 			
 			tbRow1.setWeightSum(10);
 			
 			TextView id = new TextView(this);
-			id.setText("ID " + i);
+			id.setText("Sản phẩm " + i);
 			id.setTextSize(18);
-			id.setLayoutParams(new TableRow.LayoutParams(0, LayoutParams.WRAP_CONTENT, 6f));
+			id.setLayoutParams(new TableRow.LayoutParams(0, LayoutParams.WRAP_CONTENT, 9f));
+			
+			ImageButton iconDelete = new ImageButton(this);
+			iconDelete.setImageResource(R.drawable.delete_icon);
+			
+			//ADD
+			tbRow1.addView(id);
+			tbRow1.addView(iconDelete);
+			
+			//NEW ROW
+			TableRow tbRow2 = new TableRow(this);
+			tbRow2.setLayoutParams(lp);
+			tbRow2.setWeightSum(10);
+			
+			TextView count = new TextView(this);
+			count.setText("Số lượng ");
+			count.setTextSize(18);
+			count.setLayoutParams(new TableRow.LayoutParams(0, LayoutParams.WRAP_CONTENT, 9f));
+			
+			ImageButton iconEdit = new ImageButton(this);
+			iconEdit.setImageResource(R.drawable.edit_icon);
+			
+			//ADD
+			tbRow2.addView(count);
+			tbRow2.addView(iconEdit);
+			
+			//NEW ROW
+			TableRow tbRow3 = new TableRow(this);
+			tbRow3.setLayoutParams(lp);
+			tbRow3.setWeightSum(10);
+			
+			TextView price = new TextView(this);
+			price.setText("Thành tiền " + i);
+			price.setTextSize(18);
+			price.setLayoutParams(new TableRow.LayoutParams(0, LayoutParams.WRAP_CONTENT, 9f));
+			
+			
+			
+			//ADD
+			tbRow3.addView(price);
+			
+			tb.addView(tbRow1, 0);
+			tb.addView(tbRow2, 1);
+			tb.addView(tbRow3, 2);
+			layout.addView(tb);
 			
 //			int bg= 0;
 //			if(i%2 == 0){
@@ -56,7 +112,7 @@ public class OrdersDetailManagerActivity extends Activity{
 //			id.setBackgroundResource(bg);
 //			
 //			TextView priceTotal = new TextView(this);
-//			priceTotal.setText("Gi� " + i);
+//			priceTotal.setText("Giá " + i);
 //			priceTotal.setTextSize(18);
 //			priceTotal.setLayoutParams(new TableRow.LayoutParams(0, LayoutParams.WRAP_CONTENT, 3.5f));
 //			priceTotal.setBackgroundResource(bg);

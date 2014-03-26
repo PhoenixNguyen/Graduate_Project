20b46212005597dfefbbe90bc18c3e336eb22518
hpintro <hpintro@gmail.com>
2014-03-26 18:01:47 +0700

    journey update location

--- Server/DMSProject/src/java/com/hp/dao/RoadManagementDAOImpl.java
+++ Server/DMSProject/src/java/com/hp/dao/RoadManagementDAOImpl.java
@@ -48,25 +48,17 @@ public class RoadManagementDAOImpl implements RoadManagementDAO{
                 toDatefinal = sdf2.format(toDate);
                 System.out.println(" DATECONVERT: " + datefinal + " toDatefinal " + toDatefinal);
             }
-            if(pGiamDoc != null){
-                //Lay danh sach ID khach hang cua giam doc
-                List<String> khachhang = null;
-                khachhang = session.createQuery("select kh.mMaDoiTuong "
-                        + "from Customer kh, Staff nv "
-                        + "where nv.mManager ='"+pGiamDoc+"' "
-                        + "and nv.mID = kh.mMaNhanVien ").list();
-                for(int i= 0; i < khachhang.size(); i++){
-                    List<RoadManagement> tmp = new ArrayList<RoadManagement>();
-                    if(datefinal.compareTo("") == 0)
-                        tmp = session.createQuery("from RoadManagement where mMaKhachHang='"+khachhang.get(i)+"'").list();
-                    else
-                        tmp = session.createQuery("from RoadManagement where mMaKhachHang='"+khachhang.get(i)+"'"
-                                + " and cast (mThoiGian as date) BETWEEN '"+datefinal+"' and '" +toDatefinal+"'").list();
-                    
-                    if(tmp.size() > 0)
-                        result.add(tmp);
-                }
-                
+            
+            if(pMaKhachHang != null){
+
+                List<RoadManagement> tmp = new ArrayList<RoadManagement>();
+                if(datefinal.compareTo("") == 0)
+                    tmp = session.createQuery("from RoadManagement where mMaKhachHang = '"+pMaKhachHang+"'").list();
+                else
+                    tmp = session.createQuery("from RoadManagement where mMaKhachHang = '"+pMaKhachHang+"'"
+                            + " and cast (mThoiGian as date) BETWEEN '"+datefinal+"' and '" +toDatefinal+"'").list();
+                 if(tmp != null)
+                     result.add(tmp);
             }
             else if(pNhanVien != null){
 
@@ -85,18 +77,32 @@ public class RoadManagementDAOImpl implements RoadManagementDAO{
                     if(tmp.size() > 0)
                         result.add(tmp);
                 }
+                
+                return result;
             }
-            else if(pMaKhachHang != null){
-
-                List<RoadManagement> tmp = new ArrayList<RoadManagement>();
-                if(datefinal.compareTo("") == 0)
-                    tmp = session.createQuery("from RoadManagement where mMaKhachHang = '"+pMaKhachHang+"'").list();
-                else
-                    tmp = session.createQuery("from RoadManagement where mMaKhachHang = '"+pMaKhachHang+"'"
-                            + " and cast (mThoiGian as date) BETWEEN '"+datefinal+"' and '" +toDatefinal+"'").list();
-                 if(tmp != null)
-                     result.add(tmp);
+            else 
+            if(pGiamDoc != null){
+                //Lay danh sach ID khach hang cua giam doc
+                List<String> khachhang = null;
+                khachhang = session.createQuery("select kh.mMaDoiTuong "
+                        + "from Customer kh, Staff nv "
+                        + "where nv.mManager ='"+pGiamDoc+"' "
+                        + "and nv.mID = kh.mMaNhanVien ").list();
+                for(int i= 0; i < khachhang.size(); i++){
+                    List<RoadManagement> tmp = new ArrayList<RoadManagement>();
+                    if(datefinal.compareTo("") == 0)
+                        tmp = session.createQuery("from RoadManagement where mMaKhachHang='"+khachhang.get(i)+"'").list();
+                    else
+                        tmp = session.createQuery("from RoadManagement where mMaKhachHang='"+khachhang.get(i)+"'"
+                                + " and cast (mThoiGian as date) BETWEEN '"+datefinal+"' and '" +toDatefinal+"'").list();
+                    
+                    if(tmp.size() > 0)
+                        result.add(tmp);
+                }
+                return result;
             }
+            
+            
             else{
 
                 //Lay danh sach ID khach hang cua giam doc
@@ -114,6 +120,8 @@ public class RoadManagementDAOImpl implements RoadManagementDAO{
                     if(tmp.size() > 0)
                         result.add(tmp);
                 }
+                
+                return result;
             }
 //            courses = session.createSQLQuery("select tb_quanlyduongdi.* from tb_quanlyduongdi as road,tb_nhanvien as nv  "
 //                    + "where nv.nhanvien_ma_nhan_vien=road.quanlyduongdi_ma_nhan_vien and nv.mMaNhanVien = 'DVH'").addEntity(RoadManagement.class).list();
